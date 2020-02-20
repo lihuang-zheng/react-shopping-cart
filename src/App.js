6a84be19d5d8313a9f59082f36bb4b9885d9fc26
lihuang-zheng <jerryzheng0131@gmail.com>
2020-02-20 16:59:27 -0600

    structured the app for context. also added remove item.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+// Context
+import { ProductContext } from "./contexts/ProductContext"
+import { CartContext } from "./contexts/CartContext"
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -13,28 +17,25 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart(cart => [...cart, item])
 	};
+
+	const removeItem = item => {
+		setCart(cart.filter(value => Number(value.id) !== Number(item.target.value)))
+	}
+
+	console.log("shows removeItem: ", removeItem)
+	console.log("shows cart: ", cart)
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
-
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+			<ProductContext.Provider value={{ products, addItem }}>
+				<CartContext.Provider value={{ cart, removeItem }}>
+					<Navigation cart={cart} />
+					<Route exact path="/" component={Products} />
+					<Route path="/cart" component={ShoppingCart} />
+				</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
